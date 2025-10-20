17c6b122ceadf4198be20cdacd736bb862c79879
n1wo <n.wokurka@protonmail.com>
2025-10-20 22:47:35 +0200

    ✨ feat(frontend): implement content security policy
    - add CSP to next.config.ts to improve security
    - use mediaHost variable for image and media sources
    - remove unsafe-eval in script-src
    - update @typescript-eslint packages to the latest version
    - update tailwindcss packages to the latest version

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,40 +1,31 @@
 import type { NextConfig } from "next";
 
+const mediaHost = "d2k0ncl90mug6s.cloudfront.net"; // swap to media.niwosystems.com later
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "d2k0ncl90mug6s.cloudfront.net",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: mediaHost }],
   },
-
   async headers() {
+    const csp = `
+      default-src 'self';
+      base-uri 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: https://${mediaHost};
+      media-src https://${mediaHost};
+      font-src 'self' data:;
+      connect-src 'self';
+      worker-src 'self' blob:;
+      frame-src 'none';
+      object-src 'none';
+      form-action 'self';
+      upgrade-insecure-requests;
+    `.replace(/\s{2,}/g, " ").trim();
+
     return [
-      {
-        source: "/(.*)", // apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https://d2k0ncl90mug6s.cloudfront.net;
-              media-src https://d2k0ncl90mug6s.cloudfront.net;
-              connect-src 'self';
-              font-src 'self' data:;
-              frame-src 'none';
-              object-src 'none';
-              base-uri 'self';
-              form-action 'self';
-            `.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
+      { source: "/(.*)", headers: [{ key: "Content-Security-Policy", value: csp }] },
     ];
   },
 };
