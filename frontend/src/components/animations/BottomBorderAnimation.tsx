a0462e5c1e794b7aecf2bf70f13cb689f50574b9
n1wo <n.wokurka@protonmail.com>
2025-10-22 12:07:58 +0200

    🐛 fix(animations): adjust bottom border position
    - change border position to -1px for better visual alignment

--- frontend/src/components/animations/BottomBorderAnimation.tsx
+++ frontend/src/components/animations/BottomBorderAnimation.tsx
@@ -24,7 +24,7 @@ export default function BottomBorderAnimation() {
     <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
       <div
         className="before:ease-in-expo relative h-full w-full
-                  before:absolute before:-bottom-1 before:w-0 before:border-0 
+                  before:absolute before:-bottom-px before:w-0 before:border-0 
                   before:border-content-1 before:transition-all before:duration-300
                   hover:before:w-full hover:before:border-b
                   "
