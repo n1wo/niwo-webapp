fc031d13c75a7ec18ecdd7e2f517343d84aa762f
n1wo <n.wokurka@protonmail.com>
2025-10-22 12:00:52 +0200

    💄 style(button): improve button link styling
    - refine button link appearance for better UI
    - adjust colors, border radius, and hover effects

--- frontend/src/components/nav/ButtonLink.tsx
+++ frontend/src/components/nav/ButtonLink.tsx
@@ -25,12 +25,10 @@ const ButtonLink: React.FC<ButtonLinkProps> = ({ href, children, ...props }) => 
     <Link
       href={href}
       {...props}
-      className={`inline-flex items-center justify-center rounded-md border 
-                  border-content-1 px-4 py-2 text-md font-medium border-zinc-600
-                  text-zinc-200 hover:bg-zinc-800
+      className={`inline-flex items-center justify-center rounded-2xl border 
+                  px-4 py-2 text-md font-medium border-zinc-600/40 bg-zinc-700/10 backdrop-blur-[2px]
+                  text-zinc-200 hover:bg-zinc-800/40
                   transition-colors duration-200
-                  bg-content-24/5
-                  hover:bg-content-24/10 
                   ${props.className ?? ""}`}
     >
       {children}
